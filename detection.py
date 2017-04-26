f9dfb803b472f67f0d7c93ffdeda7299eaaa81a6
lukeshirley95 <lukeshirley95@gmail.com>
2017-04-26 01:56:50 +0100

    + Creating a natural hyperparameter parent class with prediction method + Filling out the gaps in the implementation + Implementing Known-Mean and Known-Variance cases as natural hyperparameter classes + Began a hyperparameter class that will re-implement MacKay's work exactly + Added an animated graph of the run-time distribution as it changes
    BUGS:
    
    - All attempts at calculating predictive probabilities give garbage so the final result is likely to be garbage. Unclear whether this is a problem in the mathematical analysis or algorithm.

--- detection.py
+++ detection.py
@@ -9,9 +9,12 @@
 #        constant "hazard function" 1/lambda
 # hyper: hyper-parameter container object
 
-from hyperparameter import GaussianKnownMean
+from datetime import datetime
+from hyperparameter import GaussianKnownMean, NaturalGaussianKnownVariance
 from scipy import stats
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 EPS = 0.0001  # how to elegantly deal with infinity?
 
@@ -22,6 +25,8 @@ class Detector:
         self.bcon = bcon
         self.l = l
         self.hyper = hyper
+        self.r = []
+        self.prediction = []
 
     # r0 is the initial run length, output is a probability
     def boundary(self, r0):
@@ -34,54 +39,109 @@ class Detector:
         return s/z
 
     def r_initial(self):
+        # 1.
+        print("Initialize")
         r_pdf = []
         r = 0
         while self.boundary(r) > EPS:
             r_pdf.append(self.boundary(r))
             r += 1
-        return np.array(r_pdf)
+        print(r_pdf)
+        if len(r_pdf) > 1:
+            self.hyper.extend_boundary(len(r_pdf))
+        r_pdf = np.array(r_pdf)
+        self.r = [r_pdf]
+        return r_pdf
 
-    def run(self, X):
+    def new_value(self, x):
+        # 2.
+        print("Observe New Datum")
+        print(x)
+        r_pdf = self.r[-1]
 
-        # 1. Initialize
-        r_pdf = self.r_initial()
-        r_pdf_series = [r_pdf]
+        # 3.
+        print("Evaluate Predictive Probability")
+        predict = self.hyper.predictive(x)
+        print(predict)
 
-        # 2. Observe New Datum (change to a while loop for a data-stream)
-        for x in X:
+        # 4. Calculate Growth Probabilities
+        elem_mult = r_pdf * predict  # element-wise array multiplication
+        growth = elem_mult * (1-1/self.l)
 
-            # 3. Evaluate Predictive Probability
-            predict = self.hyper.predictive(x)
+        # 5. Calculate Changepoint Probabilities
+        changepoint = np.sum(elem_mult/self.l)
 
-            # 4. Calculate Growth Probabilities
-            elem_mult = r_pdf * predict  # element-wise array multiplication
-            growth = elem_mult * (1-1/self.l)
+        # 6. Calculate Total Evidence
+        evidence = changepoint + np.sum(growth)
 
-            # 5. Calculate Changepoint Probabilities
-            changepoint = np.sum(elem_mult/self.l)
+        # 7.
+        print("Determine Run Length Distribution")
+        r_pdf = np.append(changepoint, growth)/evidence
+        self.r.append(r_pdf)
+        print(r_pdf)
 
-            # 6. Calculate Total Evidence
-            evidence = changepoint + np.sum(growth)
+        # 8. Update Sufficient Statistics
+        self.hyper.update()
 
-            # 7. Determine Run Length Distribution
-            r_pdf = np.append(changepoint, growth)/evidence
-            r_pdf_series.append(r_pdf)
+        # 9. Perform Prediction
+        # self.prediction += np.sum(predict*r_pdf)
 
-            # 8. Update Sufficient Statistics
-            self.hyper.nu_update()
-            self.hyper.chi_update(x)
-
-            # 9. Perform Prediction
-            # leave until the end
+        return r_pdf
 
 
-class Parser:
-    pass
+class Visualiser(Detector):
+
+    def __init__(self, bcon, l, hyper, ax, X):
+        Detector.__init__(self, bcon, l, hyper)
+        self.line, = ax.plot([], [], 'k-')
+        self.ax = ax
+        self.X = X
+
+        # Set up plot parameters
+        self.ax.set_xlim(0, 1)
+        self.ax.set_ylim(0, 1)
+        self.ax.grid(True)
+
+        # This vertical line represents the theoretical value, to
+        # which the plotted distribution should converge.
+        # self.ax.axvline(prob, linestyle='--', color='black')
+
+    def init(self):
+        self.line.set_data([], [])
+        self.r_initial()
+        return self.line,
+
+    def __call__(self, i):
+        # This way the plot can continuously run and we just keep
+        # watching new realizations of the process
+        if i == 0:
+            return self.init()
+
+        # Choose success based on exceed a threshold with a uniform pick
+        y = self.new_value(X[i])
+        r = np.arange(y.shape[0])
+        self.ax.set_xlim(0, i)
+        self.ax.set_ylim(0, max(y))
+        self.line.set_data(r, y)
+        return self.line,
+
+
+def parser(filepath):
+    dt = np.dtype([('date', datetime), ('x', float)])
+    data = np.loadtxt(filepath, dtype=dt)
+    return [x[1] for x in data]
 
 if __name__ == "__main__":
     print("Running BOCD Demo...")
-    # X    = Parser( "dow-jones-example.dat" )
-    # gkm  = GaussianKnownMean( 0 )
-    # bocd = Detector( True, 250, gkm )
-    # bocd.run(X)
-    print("Done! (Incomplete)")
+    X = parser("watergate-djia.dat")
+    gkm = NaturalGaussianKnownVariance(0.00001)
+    # bocd = Detector(True, 250, gkm)
+    # bocd.r_initial()
+    # for x in X:
+    #     bocd.new_value(x)
+    fig, ax = plt.subplots()
+    vis = Visualiser(True, 250, gkm, ax, X)
+    anim = FuncAnimation(fig, vis, frames=len(X), init_func=vis.init,
+                         interval=100, blit=True)
+    plt.show()
+    print("Done!")
--- hyperparameter.py
+++ hyperparameter.py
@@ -1,60 +1,143 @@
 # Collection of hyperparameter classes to be used by detection.py
 
-# from scipy import stats
+from abc import ABCMeta, abstractmethod
+from scipy.special import gamma
 import numpy as np
+import math
+
+EPS = 0.0001
 
 
 class Hyper:
     # parent class-structure for all hyperparameter objects
+    __metaclass__ = ABCMeta
 
     def __init__(self):
         # effective number of observations of the prior
-        self.nu = np.array([self.nu_prior()], dtype=int)
+        self.nu = np.array([1], dtype=int)
+        self.nu_next = self.nu
         # how this contributes to the sufficient statistic
         self.chi = np.array([self.chi_prior()], dtype=float)
+        self.chi_next = self.chi
 
-    def nu_update(self):
-        self.nu = np.append(self.nu, self.nu[-1] + 1)
+    def extend_boundary(self, r_len):
+        self.nu = np.array([self.nu[0] for _ in r_len])
+        self.chi = np.array([self.chi[0] for _ in r_len])
 
-    # x is most recent data-point
-    def chi_update(self, x):
-        self.chi = np.append(self.chi, self.chi[-1] + self.suff_stat(x))
+    def adjust(self, x):
+        self.nu_next = np.append([1], self.nu + 1)
+        self.chi_next = np.append([self.chi_prior()], self.chi + self.suff_stat(x))
 
+    def update(self):
+        self.nu = self.nu_next
+        self.chi = self.chi_next
+
+    # default prior for the hyperparameter
+    @abstractmethod
+    def chi_prior(self):
+        pass
+
+    # the sufficient statistic is an updating function for the hyperparameter
+    @abstractmethod
+    def suff_stat(self, x):
+        pass
+
+    # predictive probability
+    # !!! I still haven't got this giving sensible answers for any distribution
+    @abstractmethod
     def predictive(self, x):
-        return None
-
-    @staticmethod
-    def nu_prior():
-        return None
-
-    @staticmethod
-    def chi_prior():
-        return None
-
-    @staticmethod
-    def suff_stat(x):
-        return None
+        pass
 
 
 class GaussianKnownMean(Hyper):
     # hyper-parameter methods for Gaussian-Known-Mean distribution
-    # (currently empty)
 
-    def __init__(self, mean):
+    def __init__(self, mu):
         Hyper.__init__(self)
-        self.mu = mean
+        self.mu = mu
+
+    def chi_prior(self):
+        return np.array([1, 1])
+
+    # the sufficient statistic is an updating function for the hyperparameter
+    def suff_stat(self, x):
+        a = 0.5
+        b = self.nu * (x-self.mu)**2/(2*(self.nu+1))
+        return np.array([a, b])
+
+    # predictive probability
+    def predictive(self, x):
+        pass
+
+class NaturalHyper(Hyper):
+    # parent class-structure for natural hyperparameter objects
+    # a general predictive technique can be used if we can derive f
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        Hyper.__init__(self)
 
     def predictive(self, x):
-        return None
+        self.adjust(x)
+        predict = np.array([], dtype=float)
+        for r in range(len(self.nu)):
+            f0 = self.f(self.chi[r], self.nu[r])
+            f1 = self.f(self.chi_next[r+1], self.nu_next[r+1])
+            predict = np.append(predict, self.h(x)*f0/f1)
+        return predict
 
-    @staticmethod
-    def nu_prior():
-        return None
+    # h is the normalising function of the exponential model
+    # it may or may not be a function of x
+    @abstractmethod
+    def h(self, x):
+        pass
 
+    # f is the normalising function of the conj-prior in terms of chi and nu
     @staticmethod
-    def chi_prior():
-        return None
+    @abstractmethod
+    def f(chi, nu):
+        pass
 
+
+class NaturalGaussianKnownMean(NaturalHyper):
+    # hyper-parameter methods for Gaussian-Known-Mean distribution
+
+    def __init__(self, mu):
+        Hyper.__init__(self)
+        self.mu = mu
+
+    def chi_prior(self):
+        return EPS
+
+    def h(self, x):
+        return 1/math.sqrt(2*math.pi)
+
+    # derived analytically using other results
     @staticmethod
-    def suff_stat(x):
-        return None
+    def f(chi, nu):
+        return math.sqrt((chi**(nu+2))/(2**nu))/gamma(nu/2+1)
+
+    def suff_stat(self, x):
+        return (x-self.mu)**2
+
+
+class NaturalGaussianKnownVariance(NaturalHyper):
+    # hyper-parameter methods for Gaussian-Known-Variance distribution
+
+    def __init__(self, var):
+        Hyper.__init__(self)
+        self.var = var
+
+    def chi_prior(self):
+        return 0
+
+    def h(self, x):
+        return math.exp(-x**2/(2*self.var))/math.sqrt(2*math.pi*self.var)
+
+    # derived analytically using other results
+    @staticmethod
+    def f(chi, nu):
+        return 1/(math.exp(chi**2/(2*nu))*math.sqrt(2*math.pi/nu))
+
+    def suff_stat(self, x):
+        return x/math.sqrt(self.var)
